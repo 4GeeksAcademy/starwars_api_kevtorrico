ba22aa7a8a7424ace24af8a87f9bce0d62ce7891
Kevin Cristhian Villca Torrico <kevtorricov@gmail.com>
2024-09-18 16:10:14 +0000

    Added Vehicle's and Planet's tables, relationships and classes

--- src/admin.py
+++ src/admin.py
@@ -1,18 +1,20 @@
 import os
 from flask_admin import Admin
-from models import db, User, Character, Favorite
+from models import db, User, Character, Favorite, Planet, Vehicle
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='Dragon ball API', template_mode='bootstrap3')
+    admin = Admin(app, name='STAR WARS API', template_mode='bootstrap3')
 
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Character, db.session))
     admin.add_view(ModelView(Favorite, db.session))
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Vehicle, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -29,22 +29,83 @@ class User(db.Model):
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(42), unique=True, nullable=False)
-    ki = db.Column(db.Integer, nullable=False)
-    race = db.Column(db.String(42), nullable=False)
+    height = db.Column(db.Float, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    planet_origin_id = db.Column(db.Integer, db.ForeignKey("planet.id"), nullable=False)
+    planet = db.relationship("Planet")
+    vehicle_id = db.Column(db.Integer, db.ForeignKey("vehicle.id"), nullable=False)
+    vehicle = db.relationship("Vehicle")
 
-    def __init__(self, name, ki, race):
+    def __init__(self, name, height, weight, planet, vehicle):
         self.name = name
-        self.ki = ki
-        self.race = race
+        self.height = height
+        self.weight = weight
+        self.planet = planet
+        self.vehicle = vehicle
 
 
     def serialize(self):
         return {
             "name" : self.name,
-            "ki" : self.ki,
-            "race" : self.race
+            "height" : self.height,
+            "weight" : self.weight,
+            "planet" : self.planet_origin_id,
+            "vehicle" : self.vehicle_id
         }
-    
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(42), unique=True, nullable=False)
+    density = db.Column(db.Float, nullable=False)
+    diameter = db.Column(db.Float, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    weater = db.Column(db.String(42), nullable= False)
+
+    def __init__(self, name, density, diameter, orbital_period, population, weater):
+        self.name = name
+        self.density = density
+        self.diameter = diameter
+        self.orbital_period = orbital_period
+        self.population = population
+        self.weater = weater
+
+    def serialize(self):
+        return {
+            "name" : self.name,
+            "density" : self.density,
+            "diameter" : self.diameter,
+            "orbital_period" : self.orbital_period,
+            "population" : self.population,
+            "weater" : self.weater
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(42), unique=True, nullable=False)
+    crew = db.Column(db.Integer, nullable=False)
+    model = db.Column(db.String(15), nullable=False)
+    cargo_capacity = db.Column(db.Float, nullable=False)
+    passengers = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, name, crew, model, cargo_capacity, passengers):
+        self.name = name
+        self.crew = crew
+        self.model = model
+        self.cargo_capacity = cargo_capacity
+        self.passengers = passengers
+   
+    def serialize(self):
+        return {
+            "name" : self.name,
+            "crew" : self.crew,
+            "model" : self.model,
+            "cargo_capacity" : self.cargo_capacity,
+            "passengers" : self.passengers
+        }
+
+
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
@@ -53,14 +114,24 @@ class Favorite(db.Model):
     character_id = db.Column(db.Integer, db.ForeignKey("character.id"), nullable = False)
     character = db.relationship("Character")
 
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"), nullable = False)
+    planet = db.relationship("Planet")
+
+    vehicle_id = db.Column(db.Integer, db.ForeignKey("vehicle.id"), nullable = False)
+    vehicle = db.relationship("Vehicle")
+
     #Cuando es una relacion, se construye a partir del objeto
-    def __init__(self, user, character):
+    def __init__(self, user, character, planet, vehicle):
         self.user = user
         self.character = character
+        self.planet = planet
+        self.vehicle = vehicle
 
     def serialize(self):
         return {
             "id" : self.id,
             "user": self.user_id,
-            "character" : self.character_id
+            "character" : self.character_id,
+            "planet" : self.planet_id,
+            "vehicle": self.vehicle_id
         }
